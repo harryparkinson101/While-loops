19afedb502fa8eacb06e8f9119ecfd4db03b9ce4
Harry Parkinson <71083434+harryparkinson101@users.noreply.github.com>
2022-12-12 03:02:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# while loop
+
+i = 0 
+while i < 50:
+  print(i)
+  i += 1
+else:
+  print('doone with all the work')
+
+# 2
+
+my_list = [1,2,3]
+
+i = 0 
+
+while i < len(my_list):
+  print(my_list[i])
+  i += 1
+
+# 3
+
+while True:
+  response = input('say something ')
+  if (response == 'bye'):
+    break
+
+
+
+
+
